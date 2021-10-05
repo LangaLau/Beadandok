0420da5a45587d9e569cc08b78614dd01795f0cf
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2021-10-05 12:40:56 +0200

    Form létrehozása

---
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserMaintenance.Entities
+{
+    public class User
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();      //alapértéket adni 4*8 randomszám
+
+        public string FirtsName { get; set; }
+        public string LastName { get; set; }
+
+
+        public string FullName
+        {
+            get
+            {
+                return string.Format(
+                    "{0} {1} ",
+                    LastName, FullName);
+            }
+        }
+
+    }
+}
--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -29,13 +29,89 @@ namespace UserMaintenance
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1_fullname = new System.Windows.Forms.ListBox();
+            this.label1_last = new System.Windows.Forms.Label();
+            this.label2_firstname = new System.Windows.Forms.Label();
+            this.textBox2_firstname = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1_lastname = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1_fullname
+            // 
+            this.listBox1_fullname.FormattingEnabled = true;
+            this.listBox1_fullname.Location = new System.Drawing.Point(12, 12);
+            this.listBox1_fullname.Name = "listBox1_fullname";
+            this.listBox1_fullname.Size = new System.Drawing.Size(120, 160);
+            this.listBox1_fullname.TabIndex = 0;
+            // 
+            // label1_last
+            // 
+            this.label1_last.AutoSize = true;
+            this.label1_last.Location = new System.Drawing.Point(138, 12);
+            this.label1_last.Name = "label1_last";
+            this.label1_last.Size = new System.Drawing.Size(35, 13);
+            this.label1_last.TabIndex = 3;
+            this.label1_last.Text = "label1";
+            // 
+            // label2_firstname
+            // 
+            this.label2_firstname.AutoSize = true;
+            this.label2_firstname.Location = new System.Drawing.Point(138, 45);
+            this.label2_firstname.Name = "label2_firstname";
+            this.label2_firstname.Size = new System.Drawing.Size(35, 13);
+            this.label2_firstname.TabIndex = 4;
+            this.label2_firstname.Text = "label2";
+            // 
+            // textBox2_firstname
+            // 
+            this.textBox2_firstname.Location = new System.Drawing.Point(195, 38);
+            this.textBox2_firstname.Name = "textBox2_firstname";
+            this.textBox2_firstname.Size = new System.Drawing.Size(162, 20);
+            this.textBox2_firstname.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(257, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1_lastname
+            // 
+            this.textBox1_lastname.Location = new System.Drawing.Point(195, 12);
+            this.textBox1_lastname.Name = "textBox1_lastname";
+            this.textBox1_lastname.Size = new System.Drawing.Size(162, 20);
+            this.textBox1_lastname.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(372, 206);
+            this.Controls.Add(this.textBox1_lastname);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2_firstname);
+            this.Controls.Add(this.label2_firstname);
+            this.Controls.Add(this.label1_last);
+            this.Controls.Add(this.listBox1_fullname);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1_fullname;
+        private System.Windows.Forms.Label label1_last;
+        private System.Windows.Forms.Label label2_firstname;
+        private System.Windows.Forms.TextBox textBox2_firstname;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1_lastname;
     }
 }
 
