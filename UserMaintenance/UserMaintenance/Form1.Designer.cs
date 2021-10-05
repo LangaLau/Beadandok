3069e4d6a3a41edf92b0df52bf72ef276a5a46ae
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2021-10-05 14:44:50 +0200

    added save dialog

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -33,6 +33,7 @@ namespace UserMaintenance
             this.label1_last = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1_lastname = new System.Windows.Forms.TextBox();
+            this.button2_SaveFile = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1_fullname
@@ -54,9 +55,9 @@ namespace UserMaintenance
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(259, 76);
+            this.button1.Location = new System.Drawing.Point(210, 54);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(98, 32);
+            this.button1.Size = new System.Drawing.Size(68, 32);
             this.button1.TabIndex = 7;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -69,11 +70,22 @@ namespace UserMaintenance
             this.textBox1_lastname.Size = new System.Drawing.Size(147, 20);
             this.textBox1_lastname.TabIndex = 8;
             // 
+            // button2_SaveFile
+            // 
+            this.button2_SaveFile.Location = new System.Drawing.Point(289, 54);
+            this.button2_SaveFile.Name = "button2_SaveFile";
+            this.button2_SaveFile.Size = new System.Drawing.Size(68, 32);
+            this.button2_SaveFile.TabIndex = 9;
+            this.button2_SaveFile.Text = "button2";
+            this.button2_SaveFile.UseVisualStyleBackColor = true;
+            this.button2_SaveFile.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(372, 206);
+            this.Controls.Add(this.button2_SaveFile);
             this.Controls.Add(this.textBox1_lastname);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1_last);
@@ -91,6 +103,7 @@ namespace UserMaintenance
         private System.Windows.Forms.Label label1_last;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1_lastname;
+        private System.Windows.Forms.Button button2_SaveFile;
     }
 }
 
--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,8 +21,9 @@ namespace UserMaintenance
             InitializeComponent();
 
             label1_last.Text = Resource1.FullName;
-            
+
             button1.Text = Resource1.Addbtn;
+            button2_SaveFile.Text = Resource1.WroteToFile;
 
             listBox1_fullname.DataSource = users;
             listBox1_fullname.ValueMember = "ID";
@@ -33,9 +35,23 @@ namespace UserMaintenance
         {
             var u = new User()
             {
-                FullName = textBox1_lastname.Text           
+                FullName = textBox1_lastname.Text
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {//csv írás
+            var sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() != DialogResult.OK)
+                return;
+            //mentés helye
+            using (var sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+                foreach (var u in users)
+                    sw.WriteLine(string.Format(     
+                        "{0};{1}",      //fajl nevének kiírása
+                        u.ID,
+                        u.FullName));
         }
     }
 }
