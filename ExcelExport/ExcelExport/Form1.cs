9b8cb5724ce00c3eb7e7df9c72d5c62269e01352
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2021-10-08 13:06:57 +0200

    Load Data

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -12,9 +12,26 @@ namespace ExcelExport
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();      //ORM példányosítása
+        List<Flat> lakasok;
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+            dataGridView1.DataSource = lakasok;
+        }
+
+        public void LoadData()
+        {
+            lakasok = context.Flats.ToList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'realEstateDataSet.Flat' table. You can move, or remove it, as needed.
+            this.flatTableAdapter.Fill(this.realEstateDataSet.Flat);
+
         }
     }
 }
