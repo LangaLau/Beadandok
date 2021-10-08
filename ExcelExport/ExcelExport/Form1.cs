8ec397d784a1646675a332cc0b42d2d5ab725014
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2021-10-08 14:27:31 +0200

    Excel creating & opening

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;       //Lehet = nevet adni
 using System.Reflection;
 
 namespace ExcelExport
@@ -11,6 +11,10 @@ namespace ExcelExport
     {
         RealEstateEntities context = new RealEstateEntities();      //ORM példányosítása
         List<Flat> lakasok;
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
 
         public Form1()
         {
@@ -24,10 +28,39 @@ namespace ExcelExport
             lakasok = context.Flats.ToList();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        public void CreateExcel()
         {
-           
+            try
+            {
+                xlApp = new Excel.Application();            //megnyitjuk az applikációt
+                xlWB = xlApp.Workbooks.Add(Missing.Value);  //létrehoz munkafüzet
+                xlSheet = xlWB.ActiveSheet;                 //munka lap kiválaszt
 
+                CreateTable();
+
+                xlApp.Visible = true;                       //látvány átadás 
+                xlApp.UserControl = true;                      //vezérlés felhasználonak
+            }
+            catch (Exception ex)
+            {
+                string hiba = string.Format("Error: {0}\nLine: {1}",ex.Message, ex.Source);
+                MessageBox.Show(hiba, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();       //teljesen le kell zárni
+                xlWB = null;
+                xlApp = null;
+            }
         }
+
+        private void CreateTable()
+        {
+            throw new NotImplementedException();
+        }
+
+       // private void
+
+        private void Form1_Load(object sender, EventArgs e)
+        { }
     }
 }
