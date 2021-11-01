b2be668b7f5d8dd25e891b5de0a055cc712dd4e4
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2021-11-01 16:01:21 +0100

    xml fájlba írás

--- MNBSoap/Form1.cs
+++ MNBSoap/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,16 @@ namespace MNBSoap
             request.currencyNames = "EUR";
             request.startDate = "2020-01-01";
             request.endDate = "2020-06-30";
+            // Ebben az esetben a "var" a GetExchangeRates visszatérési értékéből kapja a típusát.
+            // Ezért a response változó valójában GetExchangeRatesResponseBody típusú.
+            var response = mnbService.GetExchangeRates(request);
+
+
+            // Ebben az esetben a "var" a GetExchangeRatesResult property alapján kapja a típusát.
+            // Ezért a result változó valójában string típusú.
+            string result = response.GetExchangeRatesResult;
+            File.WriteAllText("export.xml", result);
+
         }
     }
 }
