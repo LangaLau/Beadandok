ad70bf4773df8b58cb7d3d8759840c3bdcfe4286
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2021-11-08 18:24:06 +0100

    Ball osztály létrehozása + alap beállíítások

---
+++ TervezesiMintak/TervezesiMintak/Entities/Ball.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TervezesiMintak.Entities
+{
+    class Ball : Label
+    {
+        public int méret = 50;
+
+        public Ball()
+        {
+            AutoSize = false;
+            Width = méret;
+            Height = méret;
+            Paint += Ball_Paint;
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+    }
+}
