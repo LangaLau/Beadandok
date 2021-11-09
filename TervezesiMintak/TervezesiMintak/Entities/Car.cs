aa6029387b963da4e69c5f8a56a75b811ff6e0ab
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2021-11-09 12:54:10 +0100

    Implement absztrakt osztály Car-s ba

---
+++ TervezesiMintak/TervezesiMintak/Entities/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TervezesiMintak.Abstractions;
+
+namespace TervezesiMintak.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            
+        }
+    }
+}
