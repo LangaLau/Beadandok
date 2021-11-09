31fb677e86572513b5ec588beee856ef09c7313e
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2021-11-09 13:02:14 +0100

    Car.png hozzáadás

--- TervezesiMintak/TervezesiMintak/Entities/Car.cs
+++ TervezesiMintak/TervezesiMintak/Entities/Car.cs
@@ -12,7 +12,8 @@ namespace TervezesiMintak.Entities
     {
         protected override void DrawImage(Graphics g)
         {
-            
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
         }
     }
 }
