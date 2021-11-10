ea5178dd13b8a271797f628cb68f37fb9df20678
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2021-11-10 14:31:21 +0100

    Present létrehozása

---
+++ TervezesiMintak/TervezesiMintak/Entities/Present.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TervezesiMintak.Abstractions;
+
+namespace TervezesiMintak.Entities
+{
+    class Present : Toy
+    {
+        //public Present(Color szalag; doboz);
+
+        protected override void DrawImage(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
