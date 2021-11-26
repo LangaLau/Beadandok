62ab4a3535dde6b9168c5a35f2f2e11666233170
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2021-11-26 16:32:18 +0100

    Jelszó validálás

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -26,5 +26,24 @@ namespace UnitTestExample.Test
             //Assert
             Assert.AreEqual(result, expectedResult);
         }
+
+        [
+            Test,
+            TestCase("abcd1234", false),
+             TestCase("abcdABCD", false),
+             TestCase("ABCD1234", false),
+             TestCase("abcD28", false),
+             TestCase("ABcd1234", true),
+             TestCase("HappyHipp88", true)
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            //Arrange
+            var accountController = new AccountController();
+            //Act
+            var result = accountController.ValidatePassword(password);
+            //Assert
+            Assert.AreEqual(result, expectedResult);
+        }
     }
 }
