b873ed7efd05ac7d9c9cb4de4186353947c5c753
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2021-11-26 18:01:35 +0100

    Alosztály hívás kezelése

--- UnitTestExample/UnitTestExample/Controllers/AccountController.cs
+++ UnitTestExample/UnitTestExample/Controllers/AccountController.cs
@@ -28,7 +28,9 @@ namespace UnitTestExample.Controllers
             if (!ValidatePassword(password))
                 throw new ValidationException(
                     "A megadottt jelszó nem megfelelő!\n" +
-                    "A jelszó legalább 8 karakter hosszú kell legyen, csak az angol ABC betűiből és számokból állhat, és tartalmaznia kell legalább egy kisbetűt, egy nagybetűt és egy számot.");
+                    "A jelszó legalább 8 karakter hosszú kell legyen,\n" +
+                    "csak az angol ABC betűiből és számokból állhat,\n" +
+                    "és tartalmaznia kell legalább egy kisbetűt, egy nagybetűt és egy számot.");
 
             var account = new Account()
             {
