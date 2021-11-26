6f305e1d3593eb404e732510c16ba637316780be
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2021-11-26 15:32:50 +0100

    E-mail validálás teszt függvény

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [Test]      //atributum
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
