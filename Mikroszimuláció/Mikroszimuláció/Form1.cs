40939e80d733a5c827801716cf038c91504b36f8
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2021-12-03 13:41:30 +0100

    Form1 adatok betöltése

--- Mikroszimuláció/Mikroszimuláció/Form1.cs
+++ Mikroszimuláció/Mikroszimuláció/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,13 +14,33 @@ namespace Mikroszimuláció
 {
     public partial class Form1 : Form
     {
+        List<Person> Population = null;
+        List<BirthProbability> BirthProbabilities = null;
+        List<DeathProbability> DeathProbabilities = null;
         public Form1()
         {
             InitializeComponent();
+            Population = GetPopulation(@"C:\Temp\nép-teszt.csv"); // bemenet, hol találom a fájlt
+        }
 
-            List<Person> Population = new List<Person>();
-            List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
-            List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+        public List<Person> GetPopulation(string csvPath)
+        {            //üres lista, kinyitom ,belepakolom a személyeket
+            List<Person> population = new List<Person>();
+
+            using (var sr = new StreamReader(csvPath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');  //!!!fontos különben vételen ciklus
+                    var p = new Person();
+                    p.BirthYear = int.Parse(line[0]);
+                    p.Gender =(Gender)Enum.Parse(typeof(Gender),line[1]);
+                    p.NbrOfChildren = int.Parse(line[2]);
+                    population.Add(p);
+                }
+            }
+
+            return population;
         }
     }
 }
