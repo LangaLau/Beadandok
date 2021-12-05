1c9d42ebaedc26c910a7388e0a463dc630b5d602
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2021-12-05 16:14:49 +0100

    Induló populáció felépítése

--- EvoluciosAlgoritmus/EvoluciosAlgoritmus/Form1.cs
+++ EvoluciosAlgoritmus/EvoluciosAlgoritmus/Form1.cs
@@ -15,15 +15,28 @@ namespace EvoluciosAlgoritmus
     {
         GameController gc = new GameController();
         GameArea ga;
-        
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
+
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
-            gc.AddPlayer();
+   
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
             gc.Start(true);
         }
+
+
     }
 }
